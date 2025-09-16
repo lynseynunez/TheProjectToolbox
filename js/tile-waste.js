f20bd428765491b9a873d1ee893b00ec261fdc40
lynseynunez <lynseyjuneee@gmail.com>
2025-09-16 09:35:33 -0700

    Update tile-waste.js

--- js/tile-waste.js
+++ js/tile-waste.js
@@ -6,11 +6,12 @@ function calculateTile() {
 
   if (!length || !width || !tileSize) {
     document.getElementById("result").innerText = "Please enter all values.";
+    document.getElementById("materials").innerHTML = "";
     return;
   }
 
   let area = length * width;
-  let tilesNeeded = Math.ceil((area / (tileSize * tileSize)) * (1 + waste/100));
+  let tilesNeeded = Math.ceil((area * 144) / (tileSize * tileSize) * (1 + waste / 100));
 
   document.getElementById("result").innerText = 
     `You need approximately ${tilesNeeded} tiles (including ${waste}% extra).`;
@@ -18,9 +19,9 @@ function calculateTile() {
   document.getElementById("materials").innerHTML = `
     <h3>Materials Needed:</h3>
     <ul>
-      <li>${tilesNeeded} tiles <a href="https://example.com/tiles-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
-      <li>Grout 1 bag <a href="https://example.com/grout-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
-      <li>Tile spacers <a href="https://example.com/spacers-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
+      <li>${tilesNeeded} Tiles<br><a href="https://example.com/tile-affiliate" target="_blank" class="affiliate-button">Buy Tiles</a></li>
+      <li>Grout (1 bag per 100 tiles)<br><a href="https://example.com/grout-affiliate" target="_blank" class="affiliate-button">Buy Grout</a></li>
+      <li>Tile Spacers<br><a href="https://example.com/spacer-affiliate" target="_blank" class="affiliate-button">Buy Spacers</a></li>
     </ul>
   `;
 }
