8d4d188a4309839416e39637113d2ebbbd6271d5
lynseynunez <lynseyjuneee@gmail.com>
2025-09-16 09:18:30 -0700

    Update deck-board.js

--- js/deck-board.js
+++ js/deck-board.js
@@ -10,12 +10,21 @@ function calculateDeck() {
     return;
   }
 
-  let totalWidthPerBoard = (boardWidth + spacing) / 12; // in feet
+  let totalWidthPerBoard = (boardWidth + spacing) / 12;
   let boardsAcross = Math.ceil(width / totalWidthPerBoard);
   let rowsAlong = Math.ceil(length / boardLength);
   let totalBoards = boardsAcross * rowsAlong;
-  let screws = totalBoards * 30; // rough estimate: 30 screws per board
+  let screws = totalBoards * 30;
 
   document.getElementById("result").innerText =
     `You need approximately ${totalBoards} boards and ${screws} screws.`;
+
+  document.getElementById("materials").innerHTML = `
+    <h3>Materials Needed:</h3>
+    <ul>
+      <li>${totalBoards} boards <a href="https://example.com/boards-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
+      <li>${screws} screws <a href="https://example.com/screws-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
+      <li>Deck brackets <a href="https://example.com/brackets-affiliate" target="_blank" class="affiliate-button">Buy Now</a></li>
+    </ul>
+  `;
 }
